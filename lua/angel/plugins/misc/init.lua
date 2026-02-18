46cb35411c322adbf49f30c257ac49734ef7e0c5
Angel Szymczak <angel.szymczak@pm.me>
2026-02-18 16:32:17 -0300

    fix(bootstrap): remove broken imports, fix obsidian structure
    Fixed bootstrap errors by removing imports to deleted plugins:
    - Removed gen.nvim, avante.nvim from tools/init.lua
    - Removed session-lens, highlightedyank, vim-mkdir from misc/init.lua
    - Fixed obsidian.lua structure (removed extra braces)
    
    Plugins removed (user doesn't use them):
    - session-lens.lua
    - vim-mkdir.lua
    
    Progress: Session-lens issue (#17) resolved by removal.
    
    Note: Rest.nvim has LuaRocks build error (non-blocking). Neovim
    loads successfully despite this.

--- lua/angel/plugins/misc/init.lua
+++ lua/angel/plugins/misc/init.lua
@@ -3,10 +3,7 @@
 
 return {
   { import = "angel.plugins.misc.auto-session" },
-  { import = "angel.plugins.misc.session-lens" },
   { import = "angel.plugins.misc.vim-maximizer" },
   { import = "angel.plugins.misc.sort" },
   { import = "angel.plugins.misc.tabular" },
-  { import = "angel.plugins.misc.highlightedyank" },
-  { import = "angel.plugins.misc.vim-mkdir" },
 }
--- lua/angel/plugins/tools/obsidian.lua
+++ lua/angel/plugins/tools/obsidian.lua
@@ -1,27 +1,25 @@
 return {
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    ui = {
+      enable = true,
     },
-    opts = {
-      ui = {
-        enable = true,
+    workspaces = {
+      {
+        name = "work",
+        path = "~/Vaults/Harvis",
       },
-      workspaces = {
-        {
-          name = "work",
-          path = "~/Vaults/Harvis",
-        },
-      },
-      wiki_link_func = "use_alias_only",
-      follow_url_func = function(url)
-        vim.fn.jobstart({ "open", url })
-      end,
     },
+    wiki_link_func = "use_alias_only",
+    follow_url_func = function(url)
+      vim.fn.jobstart({ "open", url })
+    end,
   },
   config = function(_, opts)
     require("obsidian").setup(opts)
@@ -30,7 +28,7 @@ return {
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "markdown",
       callback = function()
-        vim.opt.conceallevel = 2 -- or 1 for more limited animations
+        vim.opt.conceallevel = 2
       end,
     })
   end,
