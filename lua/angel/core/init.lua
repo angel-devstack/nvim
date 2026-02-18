72de7f49825046576d11469be133ec16b83c307c
Angel Szymczak <angel.szymczak@pm.me>
2026-02-18 16:25:14 -0300

    refactor(core): remove vim.notify override
    Removed vim.notify hijack that silenced which-key warnings. This
    allows real warnings to surface so keymap issues can be fixed properly.
    
    Fixes #13 from issues tracking.

--- lua/angel/core/init.lua
+++ lua/angel/core/init.lua
@@ -7,14 +7,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank({ timeout = 200 })
   end,
 })
-
--- Silenciar advertencias healthcheck de which‑key
-do
-  local orig_notify = vim.notify
-  vim.notify = function(msg, level, opts)
-    if type(msg) == "string" and msg:match("which%-key") and level == vim.log.levels.WARN then
-      return
-    end
-    return orig_notify(msg, level, opts)
-  end
-end
