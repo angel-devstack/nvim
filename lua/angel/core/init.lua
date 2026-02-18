f70a32936d1ff54b6612866075ee5cccf97a853a
Angel Szymczak <angel.szymczak@pm.me>
2026-02-18 16:22:24 -0300

    refactor(misc): replace vim-highlightedyank with built-in
    Neovim 0.5+ has vim.highlight.on_yank() built-in. Replaced the
    Vimscript plugin with a 3-line autocmd.
    
    Fixes #9 from issues tracking.

--- lua/angel/core/init.lua
+++ lua/angel/core/init.lua
@@ -1,6 +1,13 @@
 require("angel.core.options")
 require("angel.core.keymaps")
 
+-- Highlight on yank (built-in since Neovim 0.5)
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200 })
+  end,
+})
+
 -- Silenciar advertencias healthcheck de which‑key
 do
   local orig_notify = vim.notify
