b29f2a38ac5e1d6351c45efd56d64d94b0e63c06
Angel Szymczak <angel.szymczak@pm.me>
2026-02-19 03:00:16 -0300

    fix(splitjoin): correct lazy loading configuration
    - Change keys = { { gS, desc = ... }, { gJ, desc = ... } } to keys = { gS, gJ }
    - Lazy.nvim requires key specifications for on-demand loading
    - desc-only keys were causing plugin to load always
    - Now plugin loads only when gS or gJ keys are pressed

--- lua/angel/plugins/editing/splitjoin.lua
+++ lua/angel/plugins/editing/splitjoin.lua
@@ -1,7 +1,4 @@
 return {
   "AndrewRadev/splitjoin.vim",
-  keys = {
-    { "gS", desc = "Split arguments/params" },
-    { "gJ", desc = "Join arguments/params" },
-  },
+  keys = { "gS", "gJ" },
 }
